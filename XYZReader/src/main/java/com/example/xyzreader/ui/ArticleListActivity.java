cb54fac667227d0dc767d177403e5fcf1ce0aa27
David Hughes <david.j.hughes.nrxn@statefarm.com>
2017-04-25 23:02:48 -0500

    cardview, colors updates

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -60,7 +60,7 @@ public class ArticleListActivity extends ActionBarActivity implements
         toolbar = (Toolbar) findViewById(R.id.toolbar);
 
 
-        final View toolbarContainerView = findViewById(R.id.toolbar_container);
+//        final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
 
